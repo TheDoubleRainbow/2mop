ca1cb08a6877ee9b9e8466ae790fbfb7ad7233d6
Andrew <andrii.hohlov@asta.mobi>
2018-06-25 13:27:35 +0300

    migrate to vue

---
+++ mop/src/views/Jobs.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="columns is-centered">
+        <div class="column is-7">
+            <Card v-for="i in [1,2,3,4,5,6]" v-bind:key="i" />
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Card from './../components/Card/Card'
+export default {
+    name: 'jobs',
+    components: {
+        Card
+    }
+}
+</script>
---
+++ mop/src/views/Login.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class = "container loginPage">
+        <div class="columns is-multiline is-centered">
+            <LoginForm v-bind:login="login" v-if="tab === 1"  />
+            <RegisterForm v-bind:register="register" v-if="tab === 2" />
+            <RecoveryForm v-bind:recover="recover" v-bind:email="recoveryEmail" v-if="tab === 3" />
+            <div v-if="tab !== 3" class="column is-12 forgotButtonWrapper">
+                <router-link to='/recover' class='forgotButton is-dark is-outlined'>
+                  <span>Forgot your password?</span>
+                </router-link> 
+            </div>
+        </div> 
+      </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginForm from "@/components/Login/LoginForm.vue";
+import RegisterForm from "@/components/Login/RegisterForm.vue";
+import RecoveryForm from "@/components/Login/RecoveryForm.vue";
+
+export default {
+  name: "Login",
+  data: function() {
+    return {
+      tab: this.$route.path === "/login" ? 1 : this.$route.path === "/register" ? 2 : 3,
+      location: this.$route.path,
+    };
+  },
+  methods: {
+      recover(email){
+          console.log('recovering...', email)
+      },
+      login(data){
+          console.log('logining in...', data)
+      },
+      register(data){
+          console.log('registring...', data)
+      }
+  },
+  created: function() {
+  },
+  updated: function() {
+      this.tab = this.$route.path === "/login" ? 1 : this.$route.path === "/register" ? 2 : 3
+  },
+  components: {
+    LoginForm,
+    RegisterForm,
+    RecoveryForm
+  }
+};
+</script>
+
+<style>
+.errorBlock{
+        color: red;
+}
+
+.switchButton {
+  width: 100px;
+}
+
+.loginPage {
+  padding-top: 65px;
+}
+
+.login-form .field .label{
+    text-align: left;
+}
+
+.login-form{
+    padding: 20px;
+}
+
+.loginButton {
+  margin: 0 auto;
+  width: 130px;
+}
+
+.loginButtonWrap {
+  padding-top: 10px;
+  text-align: center;
+}
+
+.loginButtonWrap .or {
+  margin: 5px 30px 0px 30px;
+  display: inline-block;
+}
+
+.typeQuestion {
+  text-align: center;
+  padding-bottom: 10px;
+}
+
+.formHeader {
+  text-align: center;
+  padding-bottom: 5px;
+  font-size: 30px;
+}
+
+.forgotButtonWrapper {
+  text-align: center;
+}
+</style>
+
